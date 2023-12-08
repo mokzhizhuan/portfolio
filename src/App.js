bbfd367fc84dd2b4d435c234d2d4d6dfaec82fb6
MokZhizhuan(NeoWolf) <gamerdark44@outlook.com>
2023-12-08 15:12:43 +0800

    changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import NavBar from "./componments/navbar/NavBar.js";
 import Intro from "./componments/intro/intro.js";
 import Skills from "./componments/skills/skills.js";
+import "./style.css";
 
 function App() {
   return (
